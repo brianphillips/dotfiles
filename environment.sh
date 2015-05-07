c0b4a19420f821badc61814e7cd1fdffedb941fb
Brian Phillips <brian@thephillips.info>
2015-05-07 08:37:48 -0500

    accumulated local changes

--- environment.sh
+++ environment.sh
@@ -12,3 +12,29 @@ then
     ln -sf $SSH_AUTH_SOCK $SOCK
     export SSH_AUTH_SOCK=$SOCK
 fi
+
+if [ -z "$SSH_TTY" ]; then
+	GREP=/bin/grep
+	agent_pid=`/bin/ps -ef | $GREP ssh-agent | $GREP -v grep | /usr/bin/awk '{print $2}' | xargs`
+
+	if [ "$agent_pid" = "" ]; then
+		 # there is no agent running
+		 if [ -e "$HOME/agent.sh" ]; then
+				# remove the old file
+				/bin/rm -f $HOME/agent.sh
+		 fi
+		 # start a new agent
+		 /usr/bin/ssh-agent | $GREP -v echo >&$HOME/agent.sh
+	fi
+
+	if [ ! -e $HOME/agent.sh ]; then
+		# an agent is running, but started through some other means...
+		#	echo "export SSH_AUTH_SOCK=$(ls /tmp/ssh-*/agent.*)" > ~/agent.sh
+		#	echo "export SSH_AGENT_PID=$agent_pid" > ~/agent.sh
+	fi
+
+	test -e $HOME/agent.sh && source $HOME/agent.sh
+	if ! ssh-add -L | $GREP --quiet ssh; then
+		ssh-add
+	fi
+fi
